ecaf09a426b8f25e8fdbc8c45b8dc4ac7ebf38a2
nguyendangthao <nguyendangthaoit@gmail.com>
2022-10-19 09:59:07 +0800

    create api upload file

--- web_api/src/controllers/account.controller.ts
+++ web_api/src/controllers/account.controller.ts
@@ -1,6 +1,29 @@
 import Controller from "interfaces/controller.interface";
 import * as express from 'express';
+import * as multer from "multer";
+const upload = multer({
+    dest: 'uploads/',
+    fileFilter: (req, file, cb) => {
+        cb(null, true);
+    },
+    storage: multer.diskStorage({
+        destination: function (req, file, next) {
+            if (file.mimetype.includes('image'))
+                next(null, 'src/uploads/images');
+            else
+                next(null, 'src/uploads/files');
+        },
+        filename: function (req, file, next) {
+            if (file.mimetype.includes('image')) {
+                const ext = file.mimetype.split('/')[1];
+                next(null, "image" + '-' + Date.now() + '.' + ext);
+            } else {
+                next(null, Date.now() + '-' + file.originalname);
+            }
 
+        },
+    })
+});
 
 class AccountController implements Controller {
 
@@ -13,10 +36,24 @@ class AccountController implements Controller {
     private initializeRoutes() {
         this.router
             .get(this.path + '/detail' + '/:_id', this.detail)
+            .post(this.path + "/uploadImage", upload.single("image"), this.uploadImage)
     }
 
     private detail = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
         return response.send();
     }
+
+    private uploadImage = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
+		if (!request.file) {
+		  return response.send({
+		    status: -1,
+		    message: "don't have file",
+		  });
+		}
+		return response.send({
+		  logo_path: request.file.filename
+		});
+	};
+
 }
 export default AccountController;
